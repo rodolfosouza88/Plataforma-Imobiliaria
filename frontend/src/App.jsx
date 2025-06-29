d35402e39af3a17fb3265579109e4765f10c2840
Rodolfo Vasconcellos de Souza <rodolfovasconcellos88@gmail.com>
2025-06-29 11:24:17 -0300

    configurando ambiente front-end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
